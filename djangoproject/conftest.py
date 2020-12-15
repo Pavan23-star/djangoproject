11ff127bc15e8d77be598552df763414dc8667ba
Pavan <pavan@madhuinfotech.com>
2020-12-15 11:35:56 +0530

    first commit

---
+++ djangoproject/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from djangoproject.users.models import User
+from djangoproject.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
